532a043bb8717b628504534909d60c17fbbc8c99
Steffie21 <stefanus.oberholzer@gmail.com>
2023-03-07 18:52:24 +0200

    Sample ADC Values added

--- sampleSignal.py
+++ sampleSignal.py
@@ -6,17 +6,22 @@ T_count = 5
 
 f = 15E3 # Hz
 T = 1/f #s
-t = np.arange(0, T_count*T +T/50, T/50) # s
-signal = np.sin(2*np.pi*f*t)
+t = np.arange(0, T_count*T, T/100) # s
+signal = 1.65*np.sin(2*np.pi*f*t) +1.65
 
 Fs = 80E3 # Hz
 Ts = 1/Fs #s
 n = np.arange(0, T_count*T, Ts)
-xs = np.sin(2*np.pi*f*n)
+xs = 1.65*np.sin(2*np.pi*f*n) +1.65
+
+# print(xs)
 
 plt.plot(t, signal)
-plt.stem(n, xs)
+plt.stem(n, xs, bottom=1.65)
 plt.grid()
 plt.show()
 
-print(np.arange(0, 2*T, Ts))
+ADC_Values = np.round(xs / 3.3 *2**12)
+ADC_Values = [int(adc) for adc in ADC_Values]
+
+print(ADC_Values)
